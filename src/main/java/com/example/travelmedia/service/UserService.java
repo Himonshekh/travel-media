c80a0c88d16f167c61491f4243218d43437363c6
BS1067 <mdhimonbd111@gmail.com>
2022-03-18 17:08:33 +0600

    add mapStruct...

--- src/main/java/com/example/travelmedia/service/UserService.java
+++ src/main/java/com/example/travelmedia/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.travelmedia.service;
 
 import com.example.travelmedia.dto.RegistrationDto;
+import com.example.travelmedia.mapper.UserMapper;
 import com.example.travelmedia.model.User;
 import com.example.travelmedia.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -19,27 +20,31 @@ public class UserService {
     UserRepository userRepository;
     PasswordEncoder passwordEncoder;
 
-    public UserService(PasswordEncoder passwordEncoder){
-        this.passwordEncoder=passwordEncoder;
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public boolean isAlreadyUsedMail(String mail){
-        Optional<User> userOptional =userRepository.findByMail(mail);
-        if (userOptional.isPresent())return true;
-        return false;
+    public boolean isAlreadyUsedMail(String mail) {
+        Optional<User> userOptional = userRepository.findByMail(mail);
+        return userOptional.isPresent();
     }
-    public boolean saveRegistrationForm(RegistrationDto registrationDto){
-        if(!registrationDto.getPassword().equals(registrationDto.getConfirm()))return false;
+
+    public boolean saveRegistrationForm(RegistrationDto registrationDto) {
+        if (!registrationDto.getPassword().equals(registrationDto.getConfirm())) return false;
         log.info("userService : math password");
-        userRepository.save(registrationDto.toUser(passwordEncoder));
+        registrationDto.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+        userRepository.save(UserMapper.INSTANCE.registationToUser(registrationDto));
+//        userRepository.save(registrationDto.toUser(passwordEncoder));
         return true;
     }
-    public User fetchUserByMail(String mail){
-        Optional<User> userOptionalr = userRepository.findByMail(mail);
-        User user=null;
-        if(userOptionalr.isPresent()){
-            user=userOptionalr.get();
-        }
-        return user;
+
+    public User fetchUserByMail(String mail) {
+//        Optional<User> userOptional = userRepository.findByMail(mail);
+//        User user = null;
+        return userRepository.findByMail(mail).orElse(null);
+//        if (userOptional.isPresent()) {
+//            user = userOptional.get();
+//        }
+//        return user;
     }
 }
